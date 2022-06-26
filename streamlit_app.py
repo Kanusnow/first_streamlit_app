296dce181dfe23b5565841abaabe8ce4c9102926
Kanusnow <108201656+Kanusnow@users.noreply.github.com>
2022-06-26 10:44:39 +1000

    Update streamlit_app.py
    updating menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title ("My parent own a New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Apples")
-streamlit.text("Bananas")
-streamlit.text("Grapes")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
