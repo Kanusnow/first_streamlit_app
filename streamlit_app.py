e0185371b5f6c199dbc950c35225732034dea987
Kanusnow <108201656+Kanusnow@users.noreply.github.com>
2022-06-26 10:43:53 +1000

    Update streamlit_app.py
    Adding breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title ("My parent own a New Healthy Diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Apples")
+streamlit.text("Bananas")
+streamlit.text("Grapes")
