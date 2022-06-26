6e32d229101abc3efcba440c6d894c799b4ab527
Kanusnow <108201656+Kanusnow@users.noreply.github.com>
2022-06-26 23:23:59 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ if streamlit.button("get fruit list"):
     streamlit.dataframe(my_data_rows)
 def add_fruit_list(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values ('streamlit')")
+    my_cur.execute("insert into fruit_load_list values ('"+ new_fruit +"')")
     return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input("what fruit would you like to add?")
